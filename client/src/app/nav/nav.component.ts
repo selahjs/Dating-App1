efd09db81cbd86d26abcd9a91e90d74d1be8b9c1
selahadin <sjemal99@gmail.com>
2022-04-09 18:05:28 +0300

    End of Section 6 Created routing, Toastrs and anlso bootstrap

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -9,24 +12,29 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model: any = {}
+
+  model: any = {};
   
   // we are injecting the AccountService inside this component 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
   login()
   {
     return this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error=>{
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
+  
 
 
 }
