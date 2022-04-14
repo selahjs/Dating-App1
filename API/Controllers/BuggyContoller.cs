1ac4c97377f1e26dabb037e14651feaf01ff1bfc
selahadin <sjemal99@gmail.com>
2022-04-14 17:00:30 +0300

    End of Section 7 Error handling - Created angular interceptors Exception Middlewares and dedicated pages for displaying error messages

---
+++ API/Controllers/BuggyContoller.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+    
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if(thing == null) return NotFound();
+
+            return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+
+            var thingToRetunr = thing.ToString();
+
+            return thingToRetunr;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<AppUser> GetBadRequest()
+        {
+            return BadRequest("This was not a good request!");
+        }
+    }
+}
